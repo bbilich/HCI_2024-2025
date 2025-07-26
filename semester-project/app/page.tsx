139514d5625326d8f464e61e4ae622b08e6a25f8
Barbara Bilić <bbilich>
2025-07-26 19:15:12 +0200

    Add footer and changes to navigation bar

--- semester-project/app/page.tsx
+++ semester-project/app/page.tsx
@@ -1,8 +1,9 @@
 import Image from "next/image";
+import React from "react";
 
 export default function Home() {
   return (
-      <div>
+      <div className="bg-amber-600">
         O MENI
       </div>
   );
--- semester-project/components/navBar.tsx
+++ semester-project/components/navBar.tsx
@@ -20,8 +20,8 @@ const Navigation = () => {
     ];
 
     return(
-    <nav className="w-full px-9 py-1 bg-white shadow-sm sticky top-0 z-50">
-      <div className="max-w-8xl mx-auto flex items-center justify-between ">
+    <nav className="w-full px-9 py-1 bg-beige-200 shadow-sm sticky top-0 z-50">
+      <div className="max-w-8xl mx-auto flex items-center justify-between text-darkrose ">
         <Link href="/" className="flex items-center">
           <Image src="/logo.png" alt="Logo" width={60} height={60}/>
           <span className="font-normal text-2xl -ml-2 pt-1.5">H</span>
@@ -32,7 +32,7 @@ const Navigation = () => {
             <Link
               key={link.href}
               href={link.href}
-              className={`transition-all duration-200 hover:text-amber-700 ${
+              className={`transition-all duration-200 hover:text-beige-800 hover:font-bold ${
                 currentRoute === link.href
                   ? "border-b-2 border-black text-black"
                   : "text-gray-800"
